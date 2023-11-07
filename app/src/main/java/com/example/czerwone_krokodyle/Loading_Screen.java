43c7433ceb9a3ec0fb7d0e10b617c3ac064de056
Blaz1q <nwmjaki69@gmail.com>
2023-11-07 20:27:53 +0100

    Added text loading animation

--- app/src/main/java/com/example/czerwone_krokodyle/Loading_Screen.java
+++ app/src/main/java/com/example/czerwone_krokodyle/Loading_Screen.java
@@ -9,6 +9,7 @@ import android.os.Handler;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.daimajia.androidanimations.library.Techniques;
 import com.daimajia.androidanimations.library.YoYo;
@@ -17,12 +18,15 @@ public class Loading_Screen extends AppCompatActivity {
 
     private RelativeLayout bgimg;
     private ImageView ckrkdl;
+    Handler handler = new Handler();
+    Runnable runnable;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loading_screen);
         bgimg = findViewById(R.id.MAIN_LOADING_BG);
         ckrkdl = findViewById(R.id.krokodyl_loading);
+
         try{
             YoYo.with(Techniques.Shake).duration(1000).repeat(2).playOn(ckrkdl);
             new Handler(getMainLooper()).postDelayed(() -> {
@@ -36,6 +40,7 @@ public class Loading_Screen extends AppCompatActivity {
                         new Handler(getMainLooper()).postDelayed(() -> {
                             startActivity(new Intent(Loading_Screen.this,MainActivity.class));
                             finish();
+                            handler.removeCallbacks(runnable);
                         }, 500);
                     }, 500);
                 }, 500);
@@ -45,6 +50,30 @@ public class Loading_Screen extends AppCompatActivity {
         } catch (Exception e){
             e.printStackTrace();
         }
+    }
+    int i=0;
+    @Override
+    protected void onResume() {
+        super.onResume();
+        handler.postDelayed(runnable = new Runnable() {
+            @Override
+            public void run() {
+                handler.postDelayed(runnable, 400);
+                try{
+                    TextView loadingtext = findViewById(R.id.text_loading);
+                            String dots="";
+                            for(int j = i%3; j>=0 %3; j--){
+                                dots+=".";
+                            }
+                            String finalDots = dots;
+                            loadingtext.setText(("Loading"+ finalDots));
+
+                        i++;
+                }catch (Exception ee){
+
+                }
+            }
+        }, 400);
     }
     public void KrokodylKlikaj(View v){
         //musiałem to dodać bo crashuje w tym widoku
